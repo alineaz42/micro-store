b59ad141f5e93af1dc7d59131e6ae02e14262afe
Ali Neaz <neazali77@gmail.com>
2021-07-26 23:53:52 +0600

    completed the carousal section

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// const leftColorGrid = document.querySelector(".left-color-grid");
+// const colorBtn = document.querySelector("#color-btn");
+
+
+// colorBtn.addEventListener("click", function () {
+//     if (leftColorGrid.style.left === "0") {
+//         leftColorGrid.style.left === "-160px";
+//     } else {
+//         leftColorGrid.style.left === "0";
+//     }
+// })
